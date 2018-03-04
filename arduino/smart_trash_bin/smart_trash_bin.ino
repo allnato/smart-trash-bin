8d6220f47d0dcdec9f33da4c6928b670dfe84a3c
Allendale Nato <natoallendale@gmail.com>
2018-03-04 14:25:17 +0800

    Add tilt sensor

--- arduino/smart_trash_bin/smart_trash_bin.ino
+++ arduino/smart_trash_bin/smart_trash_bin.ino
@@ -10,6 +10,8 @@ SimpleTimer timer;
 #define TRIG_PIN  2
 #define ECHO_PIN  3
 #define DHT_PIN   4
+#define TILT_S1   5
+#define TILT_S2   6
 // CONST VAR 
 #define DIST_MAX 35
 #define DHT_TYPE DHT11
@@ -22,10 +24,12 @@ DHT dht(DHT_PIN, DHT_TYPE);
 // Function Protoypes
 void sendJSONData();
 double getSonarDistance();
-
+int getTiltPos();
 
 void setup() {
   Serial.begin(9600);
+  pinMode(TILT_S1, INPUT);
+  pinMode(TILT_S2, INPUT);
   dht.begin();
   timer.setInterval(2000, sendJSONData);
 }
@@ -43,13 +47,15 @@ void sendJSONData() {
   float sonarDistance = getSonarDistance();
   float t = dht.readTemperature();
   float h = dht.readHumidity();
-
+  int tiltPos = getTiltPos();
+  
   // Convert to JSON
   StaticJsonBuffer<200> jsonBuffer;
   JsonObject& root = jsonBuffer.createObject();
   root["sonarDistance"] = sonarDistance;
   root["temperature"] = t;
   root["humidity"] = h;
+  root["tiltPos"] = tiltPos;
   root.prettyPrintTo(Serial);
 }
 
@@ -60,5 +66,14 @@ double getSonarDistance() {
   long sonarTimeDelay = sonar.ping_median(5);
   double sonarDistance = (sonarTimeDelay / 2) / 29.154;
   return sonarDistance;
+}
+
+/**
+ * Fetch Tile Posisitoin
+ */
+int getTiltPos(){
+  int S1 = digitalRead(TILT_S1);
+  int S2 = digitalRead(TILT_S2);
+  return (S1 << 1) | S2; //BITWISE MATH
 }
 
