15a078d9b989f862a77eaefa9a5531d1b66f55fb
Allendale Nato <natoallendale@gmail.com>
2018-03-19 11:52:58 +0800

    Update real-time chart

--- server/views/js/real-time.js
+++ server/views/js/real-time.js
@@ -1,37 +1,28 @@
 var ctx = document.getElementById("myChart");
-var myChart = new Chart(ctx, {
+
+var myChart2 = new Chart($('#myChart2') ,{
     type: 'bar',
     data: {
-        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-        datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255,99,132,1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
+        labels: ['Waste Fill'],
+        datasets:[{
+            label: 'Waste height %',
+            data: [57.23],
+            backgroundColor: ['rgba(75, 192, 110, .5)'],
+            borderColor: ['rgba(75, 192, 110, 1)'],
+            borderWidth: 2
         }]
     },
     options: {
         scales: {
             yAxes: [{
+                gridLines :{display: false},
                 ticks: {
-                    beginAtZero:true
+                    beginAtZero:true,
+                    max: 100
                 }
-            }]
+            }],
+            xAxes: [{gridLines: {display: false}}]
+            
         }
     }
 });
