1d2e2551060db73804cba9a24987e2460b1d1a1f
Allendale Nato <natoallendale@gmail.com>
2018-03-25 12:52:59 +0800

    Add initial db connection

--- db/db_connect.js
+++ db/db_connect.js
@@ -1,1 +1,23 @@
 const mysql = require('mysql');
+
+// Create MYSQL connection instance.
+let conn = mysql.createConnection({
+    host            : 'localhost',
+    user            : 'root',
+    password        : 'admin123',
+    database        : 'trash',
+    connectTimeout  : 5000
+});
+
+// Establish MYSQL Connection.
+function mysqlConnect() {
+    conn.connect(err => {
+        if (err) {
+            console.log(`Error connecting to DB: ${err.message}` );
+            return;
+        }
+        console.log(`Connected to DB: ${conn.config.database}`);
+    });
+}
+
+mysqlConnect();
